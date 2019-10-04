b8f8cee0ed631a6f8342917ca2881155509856b4
Ajay1998-stack <makkapati.ajaykumar1998@gmail.com>
2019-10-04 15:46:05 +0530

    task6

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -3,14 +3,16 @@ package com.stackroute;
 
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1 =  context.getBean("mov1", Movie.class);
-        System.out.println(movie1.toString());
-        Movie movie2 =  context.getBean("mov2", Movie.class);
-        System.out.println(movie1== movie2);
+        ((AbstractApplicationContext)context).registerShutdownHook();
+//        Movie movie1 =  context.getBean("mov1", Movie.class);
+//        System.out.println(movie1.toString());
+//        Movie movie2 =  context.getBean("mov2", Movie.class);
+//        System.out.println(movie1== movie2);
       }
 }
